589c464d3b835f24ea4e3e91f8a8db372e379b2f
IfritCanCode <142740039+Ifrit42@users.noreply.github.com>
2025-08-14 11:56:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -7,4 +7,17 @@ const navLinks = document.querySelectorAll('.nav-link');
       // Add 'active' to the clicked link
       link.classList.add('active');
     });
+
   });
+
+const fadeElements = document.querySelectorAll('.fade-in');
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+      }
+    });
+  }, { threshold: 0.2 });
+
+  fadeElements.forEach(el => observer.observe(el));
